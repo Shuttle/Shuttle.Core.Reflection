c04e321ab385bf1580e4c8ffc9b83fa50a5457fe
Zavarkó Gábor <zavarkog@gmail.com>
2018-03-22 16:10:31 +0100

    Fix tabs

--- Shuttle.Core.Reflection/IReflectionService.cs
+++ Shuttle.Core.Reflection/IReflectionService.cs
@@ -6,18 +6,18 @@ namespace Shuttle.Core.Reflection
 {
     public interface IReflectionService
     {
-	    string AssemblyPath(Assembly assembly);
-		Assembly GetAssembly(string assemblyPath);
-		Assembly FindAssemblyNamed(string name);
-		IEnumerable<Assembly> GetAssemblies(string folder);
-		IEnumerable<Assembly> GetAssemblies();
+        string AssemblyPath(Assembly assembly);
+        Assembly GetAssembly(string assemblyPath);
+        Assembly FindAssemblyNamed(string name);
+        IEnumerable<Assembly> GetAssemblies(string folder);
+        IEnumerable<Assembly> GetAssemblies();
         IEnumerable<Assembly> GetMatchingAssemblies(string regex, string folder);
-		IEnumerable<Assembly> GetMatchingAssemblies(string regex);
-		IEnumerable<Assembly> GetRuntimeAssemblies();
-		IEnumerable<Type> GetTypesAssignableTo<T>();
-		IEnumerable<Type> GetTypesAssignableTo(Type type);
-		IEnumerable<Type> GetTypes(Assembly assembly);
-		IEnumerable<Type> GetTypesAssignableTo<T>(Assembly assembly);
-		IEnumerable<Type> GetTypesAssignableTo(Type type, Assembly assembly);
+        IEnumerable<Assembly> GetMatchingAssemblies(string regex);
+        IEnumerable<Assembly> GetRuntimeAssemblies();
+        IEnumerable<Type> GetTypesAssignableTo<T>();
+        IEnumerable<Type> GetTypesAssignableTo(Type type);
+        IEnumerable<Type> GetTypes(Assembly assembly);
+        IEnumerable<Type> GetTypesAssignableTo<T>(Assembly assembly);
+        IEnumerable<Type> GetTypesAssignableTo(Type type, Assembly assembly);
     }
 }
