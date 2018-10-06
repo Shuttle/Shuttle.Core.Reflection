52ee839bd7cc3b075804f5245c6e916e480090ec
eben-roux <me@ebenroux.co.za>
2018-10-06 10:20:17 +0200

    - exception fixture

---
+++ Shuttle.Core.Reflection.Tests/ExceptionExtensionsFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Shuttle.Core.Reflection.Tests
+{
+    [TestFixture]
+    public class ExceptionExtensionsFixture
+    {
+        [Test]
+        public void Should_be_able_to_find_exception()
+        {
+            var ex = new Exception();
+
+            Assert.That(ex.Find<Exception>(), Is.Not.Null);
+            Assert.That(ex.Find<InvalidOperationException>(), Is.Null);
+            Assert.That(ex.Contains<Exception>(), Is.True);
+            Assert.That(ex.Contains<InvalidOperationException>(), Is.False);
+
+            ex = new Exception(string.Empty, new InvalidOperationException());
+
+            Assert.That(ex.Find<Exception>(), Is.Not.Null);
+            Assert.That(ex.Find<InvalidOperationException>(), Is.Not.Null);
+            Assert.That(ex.Contains<Exception>(), Is.True);
+            Assert.That(ex.Contains<InvalidOperationException>(), Is.True);
+
+            ex = new Exception(string.Empty, new Exception(string.Empty, new InvalidOperationException()));
+
+            Assert.That(ex.Find<InvalidOperationException>(), Is.Not.Null);
+            Assert.That(ex.Contains<InvalidOperationException>(), Is.True);
+        }
+    }
+}
