8e84ccb7df492422fde8f161179761d9f0814e50
Eben <me@ebenroux.co.za>
2020-11-24 06:49:50 +0200

    - package

--- Shuttle.Core.Reflection/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Reflection/Properties/AssemblyInfo.cs
@@ -33,10 +33,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Standard 2.0")]
 #endif
 
-[assembly: AssemblyVersion("10.0.10.0")]
-[assembly: AssemblyCopyright("Copyright © Eben Roux 2017")]
+[assembly: AssemblyVersion("10.0.11.0")]
+[assembly: AssemblyCopyright("Copyright © Eben Roux 2019")]
 [assembly: AssemblyProduct("Shuttle.Core.Reflection")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("10.0.10")]
+[assembly: AssemblyInformationalVersion("10.0.11")]
 [assembly: ComVisible(false)]
